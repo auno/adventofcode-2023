418c274b7507a06d0bbeff245a08bd1dd9822fc6
Mikael Auno <mikael@auno.se>
2023-12-03 13:13:11 +0100

    Finished 2023/03b

--- src/day03.rs
+++ src/day03.rs
@@ -1,12 +1,13 @@
 use std::collections::HashMap;
 use aoc_runner_derive::{aoc, aoc_generator};
+use itertools::Itertools;
 use scan_fmt::scan_fmt;
 
-type Schematic = (Vec<((isize, isize), u32, usize)>, HashMap<(isize, isize), char>);
+type Schematic = (HashMap<(isize, isize), ((isize, isize), u32, usize)>, HashMap<(isize, isize), char>);
 
 #[aoc_generator(day3)]
 fn parse(input: &str) -> Schematic {
-    let mut numbers = vec![];
+    let mut numbers = HashMap::new();
     let mut symbols = HashMap::new();
 
     for (j, line) in input.lines().enumerate() {
@@ -22,7 +23,9 @@ fn parse(input: &str) -> Schematic {
                     let num_digits = number.len();
                     let number = number.parse::<u32>().unwrap();
 
-                    numbers.push(((j as isize, i as isize), number, num_digits));
+                    for k in 0..(num_digits) {
+                        numbers.insert((j as isize, (i+ k) as isize), ((j as isize, i as isize), number, num_digits));
+                    }
 
                     i += num_digits;
                 },
@@ -37,27 +40,58 @@ fn parse(input: &str) -> Schematic {
     (numbers, symbols)
 }
 
+fn neighbors(j: isize, i: isize) -> [(isize, isize); 8] {
+    [
+        (j, i - 1),
+        (j, i + 1),
+        (j - 1, i),
+        (j + 1, i),
+        (j - 1, i - 1),
+        (j - 1, i + 1),
+        (j + 1, i - 1),
+        (j + 1, i + 1)
+    ]
+}
+
 #[aoc(day3, part1)]
 fn part1(schematic: &Schematic) -> u32 {
     let (numbers, symbols) = schematic;
 
     numbers
-        .iter()
+        .values()
+        .unique()
         .filter(|((j, i), _, num_digits)| {
             (0..(*num_digits as isize))
-                .flat_map(|k| [
-                    (*j, (i+k)-1),
-                    (*j, (i+k)+1),
-                    (j-1, (i+k)),
-                    (j+1, (i+k)),
-                    (j-1, (i+k)-1),
-                    (j-1, (i+k)+1),
-                    (j+1, (i+k)-1),
-                    (j+1, (i+k)+1)
-                ])
+                .flat_map(|k| neighbors(*j, i + k))
                 .any(|neighbor| symbols.contains_key(&neighbor))
         })
         .map(|(_, number, _)| number)
+        .sum()
+}
+
+#[aoc(day3, part2)]
+fn part2(schematic: &Schematic) -> u32 {
+    let (numbers, symbols) = schematic;
+
+    symbols
+        .iter()
+        .filter(|&(_, symbol)| *symbol == '*')
+        .map(|((j, i), _)| {
+            let neighboring_numbers = neighbors(*j, *i)
+                .iter()
+                .filter_map(|neighbor| numbers.get(neighbor))
+                .unique()
+                .collect_vec();
+
+            if neighboring_numbers.len() != 2 {
+                return 0;
+            }
+
+            neighboring_numbers
+                .iter()
+                .map(|(_, number, _)| number)
+                .product::<u32>()
+        })
         .sum()
 }
 
@@ -73,5 +107,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(539713, part1(&parse(include_str!("../input/2023/day3.txt"))));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(467835, part2(&parse(include_str!("../test_input/day03.part2.467835.txt"))));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(84159075, part2(&parse(include_str!("../input/2023/day3.txt"))));
     }
 }
