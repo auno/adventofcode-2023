d02350a8a8ab2c0c889c82a1effdc967f9c06aef
Mikael Auno <mikael@auno.se>
2023-12-03 12:50:05 +0100

    Clean up day03 part 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,88 +1,64 @@
 use std::collections::HashMap;
-use anyhow::bail;
 use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::Itertools;
+use scan_fmt::scan_fmt;
 
-enum Tile {
-    Digit(u32),
-    Symbol(char),
-}
-
-impl TryFrom<char> for Tile {
-    type Error = anyhow::Error;
-
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            '.' => bail!("No such tile: {value}"),
-            '0'..='9' => Ok(Tile::Digit(value.to_digit(10).unwrap())),
-            _ => Ok(Tile::Symbol(value)),
-        }
-    }
-}
-
-type Schematic = HashMap<(isize, isize), Tile>;
+type Schematic = (Vec<((isize, isize), u32, usize)>, HashMap<(isize, isize), char>);
 
 #[aoc_generator(day3)]
 fn parse(input: &str) -> Schematic {
-    let mut schematic = Schematic::new();
+    let mut numbers = vec![];
+    let mut symbols = HashMap::new();
 
     for (j, line) in input.lines().enumerate() {
-        for (i, c) in line.chars().enumerate() {
-            if let Ok(tile) = Tile::try_from(c) {
-                schematic.insert((j as isize, i as isize), tile);
+        let mut i = 0;
+
+        while i < line.len() {
+            match line.chars().nth(i).unwrap() {
+                '.' => {
+                    i += 1;
+                },
+                '0'..='9' => {
+                    let number = scan_fmt!(&line[i..], "{d}", String).unwrap();
+                    let num_digits = number.len();
+                    let number = number.parse::<u32>().unwrap();
+
+                    numbers.push(((j as isize, i as isize), number, num_digits));
+
+                    i += num_digits;
+                },
+                symbol => {
+                    symbols.insert((j as isize, i as isize), symbol);
+                    i += 1;
+                },
             }
         }
     }
 
-    schematic
+    (numbers, symbols)
 }
 
 #[aoc(day3, part1)]
 fn part1(schematic: &Schematic) -> u32 {
-    let sorted_tiles = schematic.iter().sorted_by_key(|(&(j, i), _)| (j, i)).collect_vec();
+    let (numbers, symbols) = schematic;
 
-    let mut sum = 0;
-    let mut previous_position = (0, 0);
-    let mut number = 0;
-    let mut adjacent_symbol = false;
-
-    for (&(j, i), tile) in sorted_tiles {
-        if previous_position != (j, i - 1) {
-            if adjacent_symbol {
-                sum += number;
-                eprintln!("-- Adding {} at {:?}", number, (j, i));
-            }
-
-            number = 0;
-            adjacent_symbol = false;
-        }
-
-        if let Tile::Digit(d) = tile {
-            number = number * 10 + d;
-
-            for neighbor in [(j, i-1), (j, i+1), (j-1, i), (j+1, i), (j-1, i-1), (j-1, i+1), (j+1, i-1), (j+1, i+1)] {
-                if let Some(Tile::Symbol(_)) = schematic.get(&neighbor) {
-                    adjacent_symbol = true;
-                }
-            }
-        } else {
-            if adjacent_symbol {
-                sum += number;
-                eprintln!("-- Adding {} at {:?}", number, (j, i));
-            }
-
-            number = 0;
-            adjacent_symbol = false;
-        }
-
-        previous_position = (j, i);
-    }
-
-    if adjacent_symbol {
-        sum += number;
-    }
-
-    sum
+    numbers
+        .iter()
+        .filter(|((j, i), _, num_digits)| {
+            (0..(*num_digits as isize))
+                .flat_map(|k| [
+                    (*j, (i+k)-1),
+                    (*j, (i+k)+1),
+                    (j-1, (i+k)),
+                    (j+1, (i+k)),
+                    (j-1, (i+k)-1),
+                    (j-1, (i+k)+1),
+                    (j+1, (i+k)-1),
+                    (j+1, (i+k)+1)
+                ])
+                .any(|neighbor| symbols.contains_key(&neighbor))
+        })
+        .map(|(_, number, _)| number)
+        .sum()
 }
 
 #[cfg(test)]
