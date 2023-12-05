e0399b6e5d285ae0f574e90301a9dcf21f02c55b
Mikael Auno <mikael@auno.se>
2023-12-05 20:54:02 +0100

    Optimize day 05 part 2

--- src/day05.rs
+++ src/day05.rs
@@ -3,12 +3,11 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::Result;
 use itertools::Itertools;
 use scan_fmt::scan_fmt;
-use rayon::prelude::*;
 
-type Map = HashMap<String, (String, Vec<(u64, u64, u64)>)>;
+type Map = HashMap<String, (String, Vec<(i64, i64, i64)>)>;
 
 #[aoc_generator(day5)]
-fn parse(input: &str) -> (Vec<u64>, Map) {
+fn parse(input: &str) -> (Vec<i64>, Map) {
     let seeds = input
         .lines()
         .next()
@@ -17,17 +16,22 @@ fn parse(input: &str) -> (Vec<u64>, Map) {
         .skip(1)
         .map(str::parse)
         .map(Result::unwrap)
-        .collect::<Vec<u64>>();
+        .collect::<Vec<i64>>();
 
     let mut map = HashMap::new();
 
     for chunk in input.split("\n\n").skip(1) {
         let (source, destination) = scan_fmt!(chunk.lines().next().unwrap(), "{}-to-{} map:", String, String).unwrap();
 
-        let mappings: Vec<(u64, u64, u64)> = chunk
+        let mappings: Vec<(i64, i64, i64)> = chunk
             .lines()
             .skip(1)
-            .map(|line| scan_fmt!(line, "{d} {d} {d}", u64, u64, u64).unwrap())
+            .map(|line| scan_fmt!(line, "{d} {d} {d}", i64, i64, i64).unwrap())
+            .map(|(destination_start, source_start, len)| (
+                source_start,
+                source_start + len,
+                destination_start - source_start
+            ))
             .collect();
 
         map.insert(source.to_string(), (destination.to_string(), mappings));
@@ -37,7 +41,7 @@ fn parse(input: &str) -> (Vec<u64>, Map) {
 }
 
 #[aoc(day5, part1)]
-fn part1(input: &(Vec<u64>, Map)) -> u64 {
+fn part1(input: &(Vec<i64>, Map)) -> i64 {
     let (seeds, map) = input;
 
     seeds
@@ -49,9 +53,9 @@ fn part1(input: &(Vec<u64>, Map)) -> u64 {
                 let (next_type, mappings) = map.get(current_type).unwrap();
                 let mut next_number = current_number;
 
-                for &(dstart, sstart, len) in mappings {
-                    if (sstart..(sstart + len)).contains(&current_number) {
-                        next_number = current_number - sstart + dstart;
+                for &(start,end, diff) in mappings {
+                    if (start..end).contains(&current_number) {
+                        next_number = current_number + diff;
                         break;
                     }
                 }
@@ -67,42 +71,65 @@ fn part1(input: &(Vec<u64>, Map)) -> u64 {
 }
 
 #[aoc(day5, part2)]
-fn part2(input: &(Vec<u64>, Map)) -> u64 {
+fn part2(input: &(Vec<i64>, Map)) -> i64 {
     let (seeds, map) = input;
 
-    let mut mappings_sequence = vec![];
-
-    let mut current_type = &"seed".to_string();
-    while current_type != "location" {
-        let (next_type, mappings) = map.get(current_type).unwrap();
-        mappings_sequence.push(mappings);
-        current_type = next_type;
-    }
-
-    seeds
+    let mut mappings = seeds
         .iter()
         .tuples()
-        .flat_map(|(start, len)| *start..(start + len))
-        .par_bridge()
-        .map(|seed| {
-            let mut current_number = seed;
+        .map(|(start, len)| (*start, start + len, 0))
+        .collect_vec();
 
-            for &mappings in &mappings_sequence {
-                let mut next_number = current_number;
+    let mut category = &"seed".to_string();
 
-                for &(dstart, sstart, len) in mappings {
-                    if (sstart..(sstart + len)).contains(&current_number) {
-                        next_number = current_number - sstart + dstart;
-                        break;
-                    }
-                }
+    while category != "location" {
+        let (next_category, next_category_mappings) = map.get(category).unwrap();
 
-                current_number = next_number;
-            }
+        mappings = mappings
+            .iter()
+            .flat_map(|&mapping| {
+                next_category_mappings
+                    .iter()
+                    .fold(vec![mapping], |acc, &(ncm_start, ncm_end, _)| {
+                        acc.iter()
+                            .flat_map(|&(start, end, diff)| {
+                                if (start + diff >= ncm_start) && (end + diff <= ncm_end) {
+                                    return vec![(start, end, diff)];
+                                } else if (start + diff >= ncm_start) && (start + diff < ncm_end) {
+                                    return vec![
+                                        (start, ncm_end - diff, diff),
+                                        (ncm_end - diff, end, diff)
+                                    ];
+                                } else if (end + diff > ncm_start) && (end + diff < ncm_end) {
+                                    return vec![
+                                        (start, ncm_start - diff, diff),
+                                        (ncm_start - diff, end, diff)
+                                    ];
+                                }
 
-            current_number
-        })
-        .min()
+                                vec![(start, end, diff)]
+                            })
+                            .collect()
+                    })
+                    .iter()
+                    .map(|&(start, end, diff)| {
+                        next_category_mappings
+                            .iter()
+                            .find(|&&(ncm_start, ncm_end, _)| start + diff >= ncm_start && end + diff <= ncm_end)
+                            .map(|&(_, _, ncm_diff)| (start, end, diff + ncm_diff))
+                            .unwrap_or((start, end, diff))
+                    })
+                    .collect_vec()
+            })
+            .collect_vec();
+
+        category = next_category;
+    }
+
+    mappings
+        .iter()
+        .min_by_key(|(start, _, diff)| start + diff)
+        .map(|(start, _, diff)| start + diff)
         .unwrap()
 }
 
