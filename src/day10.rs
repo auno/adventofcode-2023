e59324d24b20313bd391f5a7525293641f3b3126
Mikael Auno <mikael@auno.se>
2023-12-10 13:08:34 +0100

    Refactor day 10 part 1

--- src/day10.rs
+++ src/day10.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
+use std::iter::successors;
 use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::{bail, Context, Error, Result};
+use itertools::Itertools;
 
 #[derive(Copy, Clone, Debug)]
 enum Pipe {
@@ -32,6 +34,23 @@ impl TryFrom<char> for Pipe {
 
 type Input = ((i32, i32), HashMap<(i32, i32), Pipe>);
 
+fn determine_starting_pipe(starting_position: &(i32, i32), map: &HashMap<(i32, i32), Pipe>) -> Result<Pipe, Error> {
+    match (
+        map.get(&(starting_position.0, starting_position.1 - 1)),
+        map.get(&(starting_position.0, starting_position.1 + 1)),
+        map.get(&(starting_position.0 - 1, starting_position.1)),
+        map.get(&(starting_position.0 + 1, starting_position.1)),
+    ) {
+        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), _, _) => Some(Pipe::Horizontal),
+        (_, _, Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest)) => Some(Pipe::Vertical),
+        (_, Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), _) => Some(Pipe::BendNorthEast),
+        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), _, Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), _) => Some(Pipe::BendNorthWest),
+        (_, Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), _, Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest)) => Some(Pipe::BendSouthEast),
+        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), _, _, Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest)) => Some(Pipe::BendSouthWest),
+        _ => None,
+    }.context("No starting position found")
+}
+
 #[aoc_generator(day10)]
 fn parse(input: &str) -> Result<Input> {
     let mut map = HashMap::new();
@@ -48,49 +67,44 @@ fn parse(input: &str) -> Result<Input> {
         }
     }
 
-    Ok((starting_position.unwrap(), map))
+    let starting_position = starting_position.context("No starting position found")?;
+    let starting_pipe = determine_starting_pipe(&starting_position, &map)?;
+    map.insert(starting_position, starting_pipe);
+
+    Ok((starting_position, map))
+}
+
+fn step(pos: (i32, i32), ppos: Option<(i32, i32)>, map: &HashMap<(i32, i32), Pipe>) -> ((i32, i32), Option<(i32, i32)>) {
+    match (map.get(&pos), ppos) {
+        (Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), None) => ((pos.0 + 1, pos.1), Some(pos)),
+        (Some(Pipe::Horizontal | Pipe::BendNorthWest), None) => ((pos.0, pos.1 + 1), Some(pos)),
+        (Some(Pipe::BendNorthEast), None) => ((pos.0, pos.1 - 1), Some(pos)),
+        (Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), Some((pj, _))) if pj != pos.0 + 1 => ((pos.0 + 1, pos.1), Some(pos)),
+        (Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest), Some((pj, _))) if pj != pos.0 - 1 => ((pos.0 - 1, pos.1), Some(pos)),
+        (Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), Some((_, pi))) if pi != pos.1 - 1 => ((pos.0, pos.1 - 1), Some(pos)),
+        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), Some((_, pi))) if pi != pos.1 + 1 => ((pos.0, pos.1 + 1), Some(pos)),
+        (pipe, _) => panic!("Nowhere to go from {:?}, coming from {:?}: {:?}", pos, ppos, pipe),
+    }
+}
+
+fn find_path(starting_position: (i32, i32), map: &HashMap<(i32, i32), Pipe>) -> Vec<(i32, i32)> {
+    successors(Some((starting_position, None)), |(position, previous_position)| {
+        let (position, previous_position) = step(*position, *previous_position, map);
+
+        if position == starting_position {
+            return None;
+        }
+
+        Some((position, previous_position))
+    }).map(|(pos, _)| pos).collect_vec()
 }
 
 #[aoc(day10, part1)]
-fn part1(input: &Input) -> Result<usize> {
-    let (spos, mut map) = input.clone();
+fn part1(input: &Input) -> usize {
+    let (starting_position, map) = input.clone();
+    let path = find_path(starting_position, &map);
 
-    let starting_pipe = match (map.get(&(spos.0, spos.1 - 1)), map.get(&(spos.0, spos.1 + 1)), map.get(&(spos.0 - 1, spos.1)), map.get(&(spos.0 + 1, spos.1))) {
-        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), _, _) => Some(Pipe::Horizontal),
-        (_, _, Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest)) => Some(Pipe::Vertical),
-        (_, Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), _) => Some(Pipe::BendNorthEast),
-        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), _, Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), _) => Some(Pipe::BendNorthWest),
-        (_, Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), _, Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest)) => Some(Pipe::BendSouthEast),
-        (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), _, _, Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest)) => Some(Pipe::BendSouthWest),
-        _ => None,
-    }.context("No starting position found")?;
-
-    map.insert(spos, starting_pipe);
-
-    let mut ppos = None;
-    let mut pos = spos;
-    let mut distance = 0usize;
-
-    loop {
-        (pos, ppos) = match (map.get(&pos), ppos) {
-            (Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), None) => ((pos.0 + 1, pos.1), Some(pos)),
-            (Some(Pipe::Horizontal | Pipe::BendNorthWest), None) => ((pos.0, pos.1 + 1), Some(pos)),
-            (Some(Pipe::BendNorthEast), None) => ((pos.0, pos.1 - 1), Some(pos)),
-            (Some(Pipe::Vertical | Pipe::BendSouthEast | Pipe::BendSouthWest), Some((pj, _))) if pj != pos.0 + 1 => ((pos.0 + 1, pos.1), Some(pos)),
-            (Some(Pipe::Vertical | Pipe::BendNorthEast | Pipe::BendNorthWest), Some((pj, _))) if pj != pos.0 - 1 => ((pos.0 - 1, pos.1), Some(pos)),
-            (Some(Pipe::Horizontal | Pipe::BendNorthWest | Pipe::BendSouthWest), Some((_, pi))) if pi != pos.1 - 1 => ((pos.0, pos.1 - 1), Some(pos)),
-            (Some(Pipe::Horizontal | Pipe::BendNorthEast | Pipe::BendSouthEast), Some((_, pi))) if pi != pos.1 + 1 => ((pos.0, pos.1 + 1), Some(pos)),
-            (pipe, _) => bail!("Nowhere to go from {:?}, coming from {:?}: {:?}", pos, ppos, pipe),
-        };
-
-        distance += 1;
-
-        if pos == spos {
-            break;
-        }
-    }
-
-    Ok(distance / 2)
+    path.len() / 2
 }
 
 #[cfg(test)]
@@ -99,16 +113,16 @@ mod tests {
 
     #[test]
     fn part1_example1() {
-        assert_eq!(4, part1(&parse(include_str!("../test_input/day10.part1.4.txt")).unwrap()).unwrap());
+        assert_eq!(4, part1(&parse(include_str!("../test_input/day10.part1.4.txt")).unwrap()));
     }
 
     #[test]
     fn part1_example2() {
-        assert_eq!(8, part1(&parse(include_str!("../test_input/day10.part1.8.txt")).unwrap()).unwrap());
+        assert_eq!(8, part1(&parse(include_str!("../test_input/day10.part1.8.txt")).unwrap()));
     }
 
     #[test]
     fn part1_input() {
-        assert_eq!(6882, part1(&parse(include_str!("../input/2023/day10.txt")).unwrap()).unwrap());
+        assert_eq!(6882, part1(&parse(include_str!("../input/2023/day10.txt")).unwrap()));
     }
 }
