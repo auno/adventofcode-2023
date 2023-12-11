e910bcd510a93337a70767e832fc30581c24c6cf
Mikael Auno <mikael@auno.se>
2023-12-11 08:32:16 +0100

    Finished 2023/11b

--- src/day11.rs
+++ src/day11.rs
@@ -21,8 +21,25 @@ fn part1(galaxies: &[(usize, usize)]) -> usize {
     galaxies
         .iter()
         .map(|&(j, i)| (
-            j + j - lines.iter().filter(|&&l| l < j).count(),
-            i + i - columns.iter().filter(|&&c| c < i).count(),
+            j + (j - lines.iter().filter(|&&l| l < j).count()),
+            i + (i - columns.iter().filter(|&&c| c < i).count()),
+        ))
+        .tuple_combinations::<(_, _)>()
+        .map(|(a, b)| a.0.abs_diff(b.0) + a.1.abs_diff(b.1))
+        .sum::<usize>()
+}
+
+#[aoc(day11, part2)]
+fn part2(galaxies: &[(usize, usize)]) -> usize {
+    let lines: HashSet<_> = galaxies.iter().map(|&(j, _)| j).collect();
+    let columns: HashSet<_> = galaxies.iter().map(|&(_, i)| i).collect();
+
+    galaxies
+        .iter()
+        .inspect(|x| eprintln!("-- > {:?}", x))
+        .map(|&(j, i)| (
+            j + 999999 * (j - lines.iter().filter(|&&l| l < j).count()),
+            i + 999999 * (i - columns.iter().filter(|&&c| c < i).count()),
         ))
         .tuple_combinations::<(_, _)>()
         .map(|(a, b)| a.0.abs_diff(b.0) + a.1.abs_diff(b.1))
@@ -41,5 +58,10 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(10885634, part1(&parse(include_str!("../input/2023/day11.txt"))));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(707505470642, part2(&parse(include_str!("../input/2023/day11.txt"))));
     }
 }
