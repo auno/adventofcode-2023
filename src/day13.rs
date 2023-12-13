274f4f0325f3ca8794f0ad2ec60f36b16bb99d4e
Mikael Auno <mikael@auno.se>
2023-12-13 13:55:20 +0100

    Finished 2023/13b

--- src/day13.rs
+++ src/day13.rs
@@ -4,7 +4,7 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::{bail, Context, Error, Result};
 use itertools::Itertools;
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 enum Tile {
     Ash,
     Rock,
@@ -24,6 +24,7 @@ impl TryFrom<char> for Tile {
     }
 }
 
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
 enum Orientation {
     Vertical,
     Horizontal,
@@ -53,7 +54,9 @@ fn parse(input: &str) -> Result<Input> {
         .collect()
 }
 
-fn find_mirror(height: usize, width: usize, pattern: &HashMap<(usize, usize), Tile>) -> Option<(Orientation, usize)> {
+fn find_mirrors(height: usize, width: usize, pattern: &HashMap<(usize, usize), Tile>) -> Vec<(Orientation, usize)> {
+    let mut mirrors = vec![];
+
     for k in 1..(width) {
         let reflection_width = min(k, width - k);
 
@@ -61,7 +64,7 @@ fn find_mirror(height: usize, width: usize, pattern: &HashMap<(usize, usize), Ti
             .cartesian_product(0..reflection_width)
             .all(|(j, l)| pattern.get(&(j, k - l - 1)) == pattern.get(&(j, k + l)))
         {
-            return Some((Orientation::Vertical, k));
+            mirrors.push((Orientation::Vertical, k));
         }
     }
 
@@ -72,11 +75,11 @@ fn find_mirror(height: usize, width: usize, pattern: &HashMap<(usize, usize), Ti
             .cartesian_product(0..reflection_height)
             .all(|(i, l)| pattern.get(&(k - l - 1, i)) == pattern.get(&(k + l, i)))
         {
-            return Some((Orientation::Horizontal, k));
+            mirrors.push((Orientation::Horizontal, k));
         }
     }
 
-    None
+    mirrors
 }
 
 #[aoc(day13, part1)]
@@ -84,11 +87,39 @@ fn part1(input: &Input) -> usize {
     input
         .iter()
         .map(|(height, width, pattern)| {
-            match find_mirror(*height, *width, pattern) {
-                Some((Orientation::Vertical, k)) => k,
-                Some((Orientation::Horizontal, k)) => k * 100,
+            match find_mirrors(*height, *width, pattern).first() {
+                Some(&(Orientation::Vertical, k)) => k,
+                Some(&(Orientation::Horizontal, k)) => k * 100,
                 None => panic!("No solution found"),
             }
+        })
+        .sum()
+}
+
+#[aoc(day13, part2)]
+fn part2(input: &Input) -> usize {
+    input
+        .iter()
+        .map(|(height, width, pattern)| {
+            let initial_mirror = *find_mirrors(*height, *width, pattern).first().unwrap();
+
+            for (&(j, i), t) in pattern {
+                let mut p2 = pattern.clone();
+                let tf = match t {
+                    Tile::Ash => Tile::Rock,
+                    Tile::Rock => Tile::Ash,
+                };
+
+                p2.insert((j, i), tf);
+
+                match find_mirrors(*height, *width, &p2).iter().find(|&&mirror| mirror != initial_mirror) {
+                    None => {}
+                    Some(&(Orientation::Vertical, k)) => { return k },
+                    Some(&(Orientation::Horizontal, k)) => { return k * 100 },
+                }
+            }
+
+            panic!("No solution found");
         })
         .sum()
 }
@@ -105,5 +136,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(34889, part1(&parse(include_str!("../input/2023/day13.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(400, part2(&parse(include_str!("../test_input/day13.part2.400.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(34224, part2(&parse(include_str!("../input/2023/day13.txt")).unwrap()));
     }
 }
