18e3de53b967b5974fc174c93cfad6ede5773912
Mikael Auno <mikael@auno.se>
2023-12-13 14:38:10 +0100

    Clean up day 13

--- src/day13.rs
+++ src/day13.rs
@@ -83,43 +83,42 @@ fn find_mirrors(height: usize, width: usize, pattern: &HashMap<(usize, usize), T
 }
 
 #[aoc(day13, part1)]
-fn part1(input: &Input) -> usize {
+fn part1(input: &Input) -> Option<usize> {
     input
         .iter()
         .map(|(height, width, pattern)| {
             match find_mirrors(*height, *width, pattern).first() {
-                Some(&(Orientation::Vertical, k)) => k,
-                Some(&(Orientation::Horizontal, k)) => k * 100,
-                None => panic!("No solution found"),
+                None => None,
+                Some(&(Orientation::Vertical, k)) => Some(k),
+                Some(&(Orientation::Horizontal, k)) => Some(k * 100),
             }
         })
         .sum()
 }
 
 #[aoc(day13, part2)]
-fn part2(input: &Input) -> usize {
+fn part2(input: &Input) -> Option<usize> {
     input
         .iter()
         .map(|(height, width, pattern)| {
-            let initial_mirror = *find_mirrors(*height, *width, pattern).first().unwrap();
+            let initial_mirror = *find_mirrors(*height, *width, pattern).first()?;
 
-            for (&(j, i), t) in pattern {
-                let mut p2 = pattern.clone();
-                let tf = match t {
+            for (&(j, i), tile) in pattern {
+                let mut modified_pattern = pattern.clone();
+
+                modified_pattern.insert((j, i), match tile {
                     Tile::Ash => Tile::Rock,
                     Tile::Rock => Tile::Ash,
-                };
+                });
 
-                p2.insert((j, i), tf);
-
-                match find_mirrors(*height, *width, &p2).iter().find(|&&mirror| mirror != initial_mirror) {
+                match find_mirrors(*height, *width, &modified_pattern).iter().find(|&&mirror| mirror != initial_mirror) {
                     None => {}
-                    Some(&(Orientation::Vertical, k)) => { return k },
-                    Some(&(Orientation::Horizontal, k)) => { return k * 100 },
+                    Some(&(Orientation::Vertical, k)) => { return Some(k) },
+                    Some(&(Orientation::Horizontal, k)) => { return Some(k * 100) },
                 }
             }
 
-            panic!("No solution found");
+            None
         })
         .sum()
 }
@@ -130,21 +129,21 @@ mod tests {
 
     #[test]
     fn part1_example1() {
-        assert_eq!(405, part1(&parse(include_str!("../test_input/day13.part1.405.txt")).unwrap()));
+        assert_eq!(405, part1(&parse(include_str!("../test_input/day13.part1.405.txt")).unwrap()).unwrap());
     }
 
     #[test]
     fn part1_input() {
-        assert_eq!(34889, part1(&parse(include_str!("../input/2023/day13.txt")).unwrap()));
+        assert_eq!(34889, part1(&parse(include_str!("../input/2023/day13.txt")).unwrap()).unwrap());
     }
 
     #[test]
     fn part2_example1() {
-        assert_eq!(400, part2(&parse(include_str!("../test_input/day13.part2.400.txt")).unwrap()));
+        assert_eq!(400, part2(&parse(include_str!("../test_input/day13.part2.400.txt")).unwrap()).unwrap());
     }
 
     #[test]
     fn part2_input() {
-        assert_eq!(34224, part2(&parse(include_str!("../input/2023/day13.txt")).unwrap()));
+        assert_eq!(34224, part2(&parse(include_str!("../input/2023/day13.txt")).unwrap()).unwrap());
     }
 }
