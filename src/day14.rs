6f9f7bfb0be5ef92e7edb23574785fce18c47046
Mikael Auno <mikael@auno.se>
2023-12-14 08:47:40 +0100

    Finished 2023/14b

--- src/day14.rs
+++ src/day14.rs
@@ -3,7 +3,7 @@ use aoc_runner_derive::{aoc, aoc_generator};
 use anyhow::{bail, Context, Error, Result};
 use itertools::Itertools;
 
-#[derive(Eq, PartialEq, Copy, Clone)]
+#[derive(Eq, PartialEq, Copy, Clone, Hash)]
 enum Rock {
     Cubed,
     Round,
@@ -61,6 +61,63 @@ fn part1(input: &Input) -> usize {
         .sum()
 }
 
+#[aoc(day14, part2)]
+fn part2(input: &Input) -> usize {
+    let (height, width, mut map) = input.clone();
+    let mut hashes = HashMap::new();
+    let mut loads = vec![];
+
+    for iteration in 0usize.. {
+        let hash = fxhash::hash32(&map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted().collect_vec());
+
+        if let Some(cycle_start) = hashes.get(&hash) {
+            let cycle_length = iteration - cycle_start;
+            let remainder = (1000000000 - cycle_start) % cycle_length;
+
+            return loads[cycle_start + remainder];
+        }
+
+        let load: usize = map.iter().filter(|&(_, &rock)| rock == Rock::Round).map(|((j, _), _)| height - j).sum();
+        hashes.insert(hash, iteration);
+        loads.push(load);
+
+
+        let round_keys = map.keys().copied().sorted().filter(|k| map.get(k) == Some(&Rock::Round)).collect_vec();
+        for (j, i) in round_keys {
+            if let Some(j1) = (0..j).rev().take_while(|&candidate| !map.contains_key(&(candidate, i))).last() {
+                map.remove(&(j, i));
+                map.insert((j1, i), Rock::Round);
+            }
+        }
+
+        let round_keys = map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted_by_key(|&(j, i)| (i, j)).collect_vec();
+        for (j, i) in round_keys {
+            if let Some(i1) = (0..i).rev().take_while(|&candidate| !map.contains_key(&(j, candidate))).last() {
+                map.remove(&(j, i));
+                map.insert((j, i1), Rock::Round);
+            }
+        }
+
+        let round_keys = map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted().rev().collect_vec();
+        for (j, i) in round_keys {
+            if let Some(j1) = ((j + 1)..height).take_while(|&candidate| !map.contains_key(&(candidate, i))).last() {
+                map.remove(&(j, i));
+                map.insert((j1, i), Rock::Round);
+            }
+        }
+
+        let round_keys = map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted_by_key(|&(j, i)| (i, j)).rev().collect_vec();
+        for (j, i) in round_keys {
+            if let Some(i1) = ((i + 1)..width).take_while(|&candidate| !map.contains_key(&(j, candidate))).last() {
+                map.remove(&(j, i));
+                map.insert((j, i1), Rock::Round);
+            }
+        }
+    }
+
+    unreachable!()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,5 +130,15 @@ mod tests {
     #[test]
     fn part1_input() {
         assert_eq!(108955, part1(&parse(include_str!("../input/2023/day14.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(64, part2(&parse(include_str!("../test_input/day14.part2.64.txt")).unwrap()));
+    }
+
+    #[test]
+    fn part2_input() {
+        assert_eq!(106689, part2(&parse(include_str!("../input/2023/day14.txt")).unwrap()));
     }
 }
