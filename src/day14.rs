115c916a21fc5de9c22dea6ee96ff4d86590defa
Mikael Auno <mikael@auno.se>
2023-12-14 11:11:34 +0100

    Clean up day 14

--- src/day14.rs
+++ src/day14.rs
@@ -43,9 +43,7 @@ fn parse(input: &str) -> Result<Input> {
         Ok((height, width, map?))
 }
 
-#[aoc(day14, part1)]
-fn part1(input: &Input) -> usize {
-    let (height, _width, mut map) = input.clone();
+fn tilt_north(mut map: HashMap<(usize, usize), Rock>) -> HashMap<(usize, usize), Rock> {
     let round_keys = map.keys().copied().sorted().filter(|k| map.get(k) == Some(&Rock::Round)).collect_vec();
 
     for (j, i) in round_keys {
@@ -55,7 +53,23 @@ fn part1(input: &Input) -> usize {
         }
     }
 
-    map.iter()
+    map
+}
+
+fn rotate(height: usize, width: usize, map: HashMap<(usize, usize), Rock>) -> (usize, usize, HashMap<(usize, usize), Rock>) {
+    let map = map.iter()
+        .map(|(&(j, i), &rock)| ((i, height - j - 1), rock))
+        .collect();
+
+    (width, height, map)
+}
+
+#[aoc(day14, part1)]
+fn part1(input: &Input) -> usize {
+    let (height, _width, map) = input.clone();
+
+    tilt_north(map)
+        .iter()
         .filter(|&(_, &rock)| rock == Rock::Round)
         .map(|((j, _), _)| height - j)
         .sum()
@@ -68,7 +82,13 @@ fn part2(input: &Input) -> usize {
     let mut loads = vec![];
 
     for iteration in 0usize.. {
-        let hash = fxhash::hash32(&map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted().collect_vec());
+        let hash = fxhash::hash32(
+            &map.keys()
+                .copied()
+                .filter(|k| map.get(k) == Some(&Rock::Round))
+                .sorted()
+                .collect_vec()
+        );
 
         if let Some(cycle_start) = hashes.get(&hash) {
             let cycle_length = iteration - cycle_start;
@@ -81,38 +101,10 @@ fn part2(input: &Input) -> usize {
         hashes.insert(hash, iteration);
         loads.push(load);
 
-
-        let round_keys = map.keys().copied().sorted().filter(|k| map.get(k) == Some(&Rock::Round)).collect_vec();
-        for (j, i) in round_keys {
-            if let Some(j1) = (0..j).rev().take_while(|&candidate| !map.contains_key(&(candidate, i))).last() {
-                map.remove(&(j, i));
-                map.insert((j1, i), Rock::Round);
-            }
-        }
-
-        let round_keys = map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted_by_key(|&(j, i)| (i, j)).collect_vec();
-        for (j, i) in round_keys {
-            if let Some(i1) = (0..i).rev().take_while(|&candidate| !map.contains_key(&(j, candidate))).last() {
-                map.remove(&(j, i));
-                map.insert((j, i1), Rock::Round);
-            }
-        }
-
-        let round_keys = map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted().rev().collect_vec();
-        for (j, i) in round_keys {
-            if let Some(j1) = ((j + 1)..height).take_while(|&candidate| !map.contains_key(&(candidate, i))).last() {
-                map.remove(&(j, i));
-                map.insert((j1, i), Rock::Round);
-            }
-        }
-
-        let round_keys = map.keys().copied().filter(|k| map.get(k) == Some(&Rock::Round)).sorted_by_key(|&(j, i)| (i, j)).rev().collect_vec();
-        for (j, i) in round_keys {
-            if let Some(i1) = ((i + 1)..width).take_while(|&candidate| !map.contains_key(&(j, candidate))).last() {
-                map.remove(&(j, i));
-                map.insert((j, i1), Rock::Round);
-            }
-        }
+        map = (0..4).fold((height, width, map), |(height, width, map), _| {
+            let map = tilt_north(map);
+            rotate(height, width, map)
+        }).2;
     }
 
     unreachable!()
