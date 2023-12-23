f1bd72f5a583af8fd50f9b8c67ab65992c8557bb
Mikael Auno <mikael@auno.se>
2023-12-23 15:17:59 +0100

    Finished 2023/23b

--- src/day23.rs
+++ src/day23.rs
@@ -1,9 +1,11 @@
-use std::collections::{BinaryHeap, HashMap};
+use std::cmp::max;
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::fmt::Debug;
 use std::hash::Hash;
 use aoc_runner_derive::{aoc, aoc_generator};
 use strum_macros::EnumIter;
 use anyhow::{bail, Context, Error, Result};
+use itertools::Itertools;
 use strum::IntoEnumIterator;
 
 #[derive(Copy, Clone, Eq, PartialEq, EnumIter, Ord, PartialOrd, Hash, Debug)]
@@ -153,6 +155,92 @@ fn part1((height, width, map): &Input) -> Option<usize> {
     distance((source, Direction::East), neighbors, arrived)
 }
 
+type Connections = HashMap<(usize, usize), Vec<((usize, usize), usize)>>;
+fn dfs(connections: &Connections, source: (usize, usize), target: (usize, usize)) -> usize {
+    type Cache = HashMap<(Vec<(usize, usize)>, usize), usize>;
+    fn dfs_impl(cache: &mut Cache, connections: &Connections, path: &mut Vec<(usize, usize)>, target: (usize, usize), distance: usize) -> usize {
+        let current_position = path.last().unwrap();
+        let cache_key = (path.iter().copied().sorted().collect_vec(), distance);
+
+        if let Some(cached_distance) = cache.get(&cache_key) {
+            return *cached_distance;
+        }
+
+        if *current_position == target {
+            return distance;
+        }
+
+        let neighbors = connections
+            .get(current_position)
+            .unwrap()
+            .iter()
+            .filter(|(neighbor_position, _)| !path.contains(neighbor_position))
+            .collect_vec();
+
+        let mut best = 0;
+
+        for (neighbor_position, neighbor_distance_increment) in neighbors {
+            path.push(*neighbor_position);
+            best = max(best, dfs_impl(cache, connections, path, target, distance + neighbor_distance_increment));
+            path.pop();
+        }
+
+        cache.insert(cache_key, best);
+        best
+    }
+
+    dfs_impl(&mut HashMap::new(), connections, &mut vec![source], target, 0)
+}
+
+#[aoc(day23, part2)]
+fn part2((height, _, map): &Input) -> usize {
+    let source = map.iter().find(|&(&(j, _), &v)| v == Tile::Path && j == 0).map(|(&k, _)| k).unwrap();
+    let target = map.iter().find(|&(&(j, _), &v)| v == Tile::Path && j == (height - 1)).map(|(&k, _)| k).unwrap();
+
+    let mut connections = HashMap::new();
+    let mut queue = VecDeque::from_iter([(source, Direction::South.step(source).unwrap(), Direction::South)]);
+    let mut visited = HashSet::from([source]);
+
+    while let Some((run_source, position, direction)) = queue.pop_front() {
+        visited.insert(position);
+        let mut distance = 1usize;
+        let mut current_position = position;
+        let mut current_direction = direction;
+
+        loop {
+            let neighbors = Direction::iter()
+                .filter(|&neighbor_direction| neighbor_direction != current_direction.reverse())
+                .filter_map(|neighbor_direction| Some((neighbor_direction.step(current_position)?, neighbor_direction)))
+                .filter(|(neighbor_position, _)| match map.get(neighbor_position) {
+                    Some(Tile::Path | Tile::Slope(_)) => true,
+                    None | Some(Tile::Forest) => false,
+                })
+                .collect_vec();
+
+            match neighbors.len() {
+                1 => {
+                    (current_position, current_direction) = neighbors[0];
+                    distance += 1;
+                },
+                _ => {
+                    connections.entry(run_source).or_insert_with(Vec::new).push((current_position, distance));
+                    connections.entry(current_position).or_insert_with(Vec::new).push((run_source, distance));
+
+                    for (neighbor_position, neighbor_direction) in neighbors {
+                        if !visited.contains(&neighbor_position) {
+                            queue.push_back((current_position, neighbor_position, neighbor_direction));
+                        }
+                    }
+
+                    break;
+                },
+            }
+        }
+    }
+
+    dfs(&connections, source, target)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -166,4 +254,14 @@ mod tests {
     fn part1_input() {
         assert_eq!(2278, part1(&parse(include_str!("../input/2023/day23.txt")).unwrap()).unwrap());
     }
+
+    #[test]
+    fn part2_example1() {
+        assert_eq!(154, part2(&parse(include_str!("../test_input/day23.example1.txt")).unwrap()));
+    }
+
+    // #[test]
+    // fn part2_input() {
+    //     assert_eq!(6734, part2(&parse(include_str!("../input/2023/day23.txt")).unwrap()).unwrap());
+    // }
 }
